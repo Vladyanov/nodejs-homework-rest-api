c7e7d34fc76625f948d033de7581dabcd85af536
Vlad Pazyna <vladpazyna@gmail.com>
2023-04-22 15:14:15 +0300

    auth done

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 const DB_HOST =
   "mongodb+srv://Vlad:9rCgcf3H2ZWVfOjz@cluster0.pt1do6i.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
+const SECRET_KEY = "u^;abc!@#7gYY%";
+
 module.exports = {
   DB_HOST,
+  SECRET_KEY,
 };
